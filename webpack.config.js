ca4c6dfad9cbd3eb4a291fd92f867c288b84f317
Marvin <frankem@uni-bremen.de>
2022-07-11 00:57:40 +0200

    init npm and webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: 'public'
+                }
+            ]
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    }
+};
